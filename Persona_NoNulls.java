22b2b9ca8aa3522b3dd90cfe02a8b16068a45818
Bri-ght <51532257+Bri-ght@users.noreply.github.com>
2020-10-23 19:37:00 +0200

    First version
    Ended, selectors left

---
+++ Persona_NoNulls.java
@@ -0,0 +1,274 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.File;
+import java.io.FileWriter;
+
+public class Persona_NoNulls{
+    int nac;
+    int fac;
+    int gen; // 0 -> HOMBRE, 1 -> MUJER
+    String nom;
+    
+    public static Persona[] leeFichero(String nomfich) throws IOException{
+        BufferedReader br = new BufferedReader (new FileReader(new File(nomfich)),131072);
+        int n = Integer.parseInt(br.readLine());
+        System.out.println("_|____|____|____|____|");
+        long t0, t1, t2;
+        t0 = System.nanoTime();
+        Persona[] dat = new Persona[n]; 
+        for(int i = 0; i<n; i++){
+            dat[i]=new Persona();
+        }
+        t1 = System.nanoTime()-t0;
+        System.out.print("*");
+        int lim = n/20;
+        t0 = System.nanoTime();
+        for(int i = 0; i<n; i++){
+            dat[i].parse(br.readLine(),br.readLine());
+            if(i % lim == lim - 1){
+                System.out.print("*");
+            }
+        }
+        t2 = System.nanoTime() - t0;
+        System.out.printf("\nCreación array: %.5f seg. Lectura fichero: %.5f seg.\n", 1e-9*t1, 1e-9*t2);
+        System.out.printf("n = %d personas en total.\n\n",dat.length);
+        return dat;
+    }
+
+    public void parse(String lin1, String lin2){
+        nac = (lin1.charAt(0)-48) *10000 + (lin1.charAt(1)-48)*1000 +
+              (lin1.charAt(2)-48) *100 + (lin1.charAt(3)-48)*10 +
+              (lin1.charAt(4)-48);
+        fac = (lin1.charAt(6)-48) *10000 + (lin1.charAt(7)-48)*1000 +
+              (lin1.charAt(8)-48) *100 + (lin1.charAt(9)-48)*10 +
+              (lin1.charAt(10)-48);
+        gen = lin1.charAt(12)-48;
+        nom = lin2;
+    }
+
+    public static int traduceFecha(String fec){
+        String[] trozos = fec.split("/");
+        int dia = Integer.parseInt(trozos[0]);
+        int mes = Integer.parseInt(trozos[1]);
+        int ano = Integer.parseInt(trozos[2]);
+        return 367*ano - (7*(ano+5001+(mes-9)/7))/4 + (275*mes)/9 + dia - 692561;
+    }
+
+    public static Peers[] makebigger(Peers[] old){
+        int len = old.length;
+        Peers[] big = new Peers[2*old.length];
+        for (int i = 0; i < len; i++){
+            big[i] = old[i];
+        }
+        return big;
+    }
+
+    public static Peers[] process(int min, int max, Persona[] arr, Counter filt, Counter ins){
+        Peers[] processed = new Peers[100];
+        int k = 0;
+        int len = arr.length;
+        long t0, t1;
+        for (int i = 0; i < len; i++){
+            boolean flag = false;
+            Persona person = arr[i];
+            filt.compsPlusOne();
+            t0 = System.nanoTime();
+            if (min <= person.nac && max >= person.nac){
+                t1 = System.nanoTime();
+                filt.addTime(t1-t0);
+                // BUSCAR EN Peers SI HAY ESE NOMBRE
+                t0 = System.nanoTime();
+                for (int j = 0; j < k; j++){
+                    ins.compsPlusOne();
+                    if (person.nom.equals(processed[j].getname())){
+                        processed[j].plus();
+                        flag = true;
+                        break;
+                    }
+                }
+                if (!flag){
+                    ins.movesPlusOne();
+                    processed[k] = new Peers(person.nom);
+                    k += 1;
+                    if (k == processed.length) {
+                        ins.addMoves(k);
+                        processed = makebigger(processed);
+                    }
+                }
+                t1 = System.nanoTime();
+                ins.addTime(t1-t0);
+            }
+            // CONTEO SI NO ENTRA?
+        }
+        /* Relleno con personas llamadas empty y numeros negativos,
+        *  Hecho así para no tener que tratar NullPointerExceptions
+         */
+        // TODO k (nombres distintos)
+        // t2_comps = nanosegundos de FILTRADO
+        return processed;
+    }
+
+    public static void sort(Peers[] v, int len, Counter counter){
+        long t0 = System.nanoTime();
+        sortRec(v, 0, len, counter);
+        long t1 = System.nanoTime();
+        counter.addTime(t1-t0);
+    }
+
+    public static void sortRec(Peers[] v, int low, int hi, Counter counter){
+        if (low >= hi){return;}
+        int lim = partition(v, low, hi, counter);
+        sortRec(v, low, lim-1, counter);
+        sortRec(v, lim+1, hi, counter);
+    }
+
+    public static int partition(Peers[] v, int low, int hi, Counter counter){
+        // Pivote inicial v[low]
+        // Empiezo a comprobar en el siguiente low+1
+        int comps=0, movs=0;
+        int lim = low+1;
+        for (int i = low+1; i < hi; i++){
+            counter.compsPlusOne();
+            counter.compsPlusOne();
+            if (v[i].getnumber() > v[low].getnumber()) {
+                counter.movesPlusOne();
+                Peers temp = new Peers(v[i].getname(), v[i].getnumber());
+                v[i] = v[lim];
+                v[lim] = temp;
+                lim++;
+            }
+        }
+        counter.movesPlusOne();
+        Peers temp = v[low];
+        v[low] = v[lim-1];
+        v[lim-1] =  temp;
+        // TODO GUARDAR EN ARCHIVO?
+        // System.out.printf("comps:%d, movs:%d\n",comps,movs);
+        return lim - 1;
+    }
+
+    public static void main(String[] args){
+        Persona[] persons = new Persona[0];
+        try{
+            persons = leeFichero("personas_va.txt");
+        }
+        catch(IOException e){
+            System.out.println("Error en la apertura del archivo.");
+        }
+        int NUMERO = 5;
+        String NOMBRE = "CESAR";
+        int comp = 0;
+        //TODO CAMBIAR FINALS Y A MAYUSCULAS EL NOMBRE
+        //TODO comp es el numero de comparaciones para encontrar el nombre
+        String date = "1/1/1974 31/12/1982";
+        String[] splitdate = date.split(" ");
+        int datemin = traduceFecha(splitdate[0]);
+        int datemax = traduceFecha(splitdate[1]);
+
+        Counter filtrado = new Counter();
+        Counter insercion = new Counter();
+        Counter extraction = new Counter();
+        Peers[] data = process(datemin, datemax, persons, filtrado, insercion);
+
+        long t0;
+        int temp;
+        int size = 0;
+        try{
+            for (int i = 0; i < data.length; i++){
+                temp = data[i].getnumber();
+                size += 1;
+            }
+        }catch(NullPointerException n){}
+        sort(data, size, extraction);
+        long sort_time = System.nanoTime();
+        long search_time;
+        for (int i = 0; i < 5; i++) {
+            System.out.print(i + 1 + " " + data[i].getname() + " " + data[i].getnumber());
+            System.out.println();
+        }
+        int different_names = filtrado.getComps();
+        int search_comps = 0;
+        t0 = System.nanoTime();
+        for (int i = 0; i < different_names; i++) {
+            search_comps += 1;
+            if (data[i].getname().equals(NOMBRE)) {
+                System.out.println(i + 1 + " " + data[i].getname() + " " + data[i].getnumber());
+                break;
+            }
+        }
+        search_time = System.nanoTime();
+        search_time -= t0;
+        System.out.printf("Filtrado\t%d comps\t%d moves\t%.5f sec\n",filtrado.getComps(), filtrado.getMoves(), 1e-9*filtrado.getTime());
+        System.out.printf("Insercion\t%d comps\t%d moves\t%.5f sec\n",insercion.getComps(), insercion.getMoves(), 1e-9*insercion.getTime());
+        System.out.printf("Extraccion\t%d comps\t%d moves\t%.5f sec\n",extraction.getComps(), extraction.getMoves(),1e-9*extraction.getTime());
+        System.out.printf("Seguimiento\t%d comps\t%d moves\t%.5f sec\n",search_comps, 0, 1e-9*search_time);
+    }
+}
+
+class Peers{
+    private String name;
+    private int number;
+
+    Peers(String a){
+        name = a;
+        number = 1;
+    }
+
+    Peers(String a, int b){
+        name = a;
+        number = b;
+    }
+
+    void plus(){
+        number += 1;
+    }
+
+    String getname(){
+        return name;
+    }
+    
+    int getnumber(){
+        return number;
+    }
+}
+
+class Counter{
+    private int moves;
+    private int comps;
+    private long time;
+
+    public Counter(){
+        this.time = 0;
+        this.moves = 0;
+        this.comps = 0;
+    }
+
+    public void movesPlusOne(){
+        moves += 1;
+    }
+
+    public void addMoves(int a){
+        moves += a;
+    }
+
+    public void compsPlusOne(){
+        comps += 1;
+    }
+
+    public int getMoves(){
+        return moves;
+    }
+
+    public int getComps(){
+        return comps;
+    }
+
+    public long getTime(){
+        return time;
+    }
+
+    public void addTime(long a){
+        time += a;
+    }
+}
